e7e75297affc0df3c53d2664f5d370d3b05b2820
Mohammad Rifat Arefin <rifatarefin19@gmail.com>
2023-12-27 19:11:52 +0600

    update Readme

--- search.py
+++ search.py
@@ -82,7 +82,7 @@ def main_internal(external_folder, log_file, random_guides=False):
     if random_guides:
         guide_folder = os.path.join(external_folder, "random-guides")
     else:
-        guide_folder = os.path.join(external_folder, "guides-big")
+        guide_folder = os.path.join(external_folder, "guides-debug")
     parser_command = os.path.join(external_folder, f"parse_{bench_name}")
 
     main(parser_command, guide_folder, log_file)
